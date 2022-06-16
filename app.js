d0974d21e09a17697671531e2a6b495c44374380
AzharSayyed <azhartsiddiqui1234@gmail.com>
2022-06-16 16:40:36 +0530

    filter function

---
+++ app.js
@@ -0,0 +1,8 @@
+module.exports = {
+  x: 19,
+  y: 20,
+
+  myName:function(){
+    return "azhar"
+  }
+};
--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-let a = 10;
-let b = 20;
-console.log(a==b);
+let app = require("./app");
+
+let mynum = [1,2,3,4,5,6,7,7,9,0]
+
+// for(let i=0;i<mynum.length;i++){
+//      console.log(mynum[i]>2)
+// }
+
+let result = mynum.filter((item)=>{
+    return item>2;
+})
+
+console.log(result);
